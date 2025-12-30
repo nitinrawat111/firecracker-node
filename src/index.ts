8c9705f903937d38ed5464240f56e1de007854b0
nitinrawat111 <rawat.nitin111@gmail.com>
2025-12-30 22:10:11 +0530

    [chore]: update tsconfig to preserve comments + setup package config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import { FirecrackerAPIClient } from "./api/api-client";
-import { InstanceActions } from "./types/api";
+export * from "./api/api-client";
+export * from "./types/api";
 
-// TODO: Export relevant things here
-// This is just an example usage of the API client. Added temporarily until the testing is setup
-const api = new FirecrackerAPIClient("/tmp/firecracker.socket");
-async function main() {
-  await api.setBootSource({
-    kernel_image_path: "/home/azios/Downloads/hello-vmlinux.bin",
-    boot_args: "console=ttyS0 reboot=k panic=1 pci=off",
-  });
+// import { FirecrackerAPIClient } from "./api/api-client";
+// import { InstanceActions } from "./types/api";
 
-  await api.createOrUpdateDrive({
-    drive_id: "rootfs",
-    path_on_host: "/home/azios/Downloads/hello-rootfs.ext4",
-    is_root_device: true,
-    is_read_only: false,
-  });
+// // This is just an example usage of the API client. Added temporarily until the testing is setup
+// async function main() {
+//   const api = new FirecrackerAPIClient("/tmp/firecracker.socket");
+//   await api.setBootSource({
+//     kernel_image_path: "/home/azios/Downloads/hello-vmlinux.bin",
+//     boot_args: "console=ttyS0 reboot=k panic=1 pci=off",
+//   });
 
-  await api.startAction(InstanceActions.InstanceStart);
-}
-main();
+//   await api.createOrUpdateDrive({
+//     drive_id: "rootfs",
+//     path_on_host: "/home/azios/Downloads/hello-rootfs.ext4",
+//     is_root_device: true,
+//     is_read_only: false,
+//   });
+
+//   await api.startAction(InstanceActions.InstanceStart);
+// }
+// main();

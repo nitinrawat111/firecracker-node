089b5a3ce1748d06775a0e6cc11a2297fa9f3427
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-05 00:44:30 +0530

    [chore]: export missed types + version bump + decsription udpate

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./core/firecracker-microvm-launcher";
 export * from "./core/firecracker-microvm";
 export * from "./api/api-client";
 export * from "./types/api";
+export * from "./types/firecracker";
